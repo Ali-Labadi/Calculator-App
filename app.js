8a708ab070cc46515c0642e0bb59552e6a11ebb4
Ali_Labadi <ali.mohammed.labadi@gmail.com>
2023-05-19 20:07:40 +0300

    clear button and adding to input value

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const input = document.querySelector('input')
+const input = document.querySelector("input");
+const addBtn = document.getElementById("addition");
+const clearBtn = document.getElementById("clear");
+
+addBtn.addEventListener("click", () => {
+  input.value += "+";
+});
+
+clearBtn.addEventListener("click", () => {
+  input.value = "";
+});
 
 function addNumsToInput(num) {
-    input.value += num
+  input.value += num;
 }
