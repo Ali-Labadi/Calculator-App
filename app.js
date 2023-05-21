fa3cedb42f40a872b9c42728a97670e09258cf85
Ali_Labadi <ali.mohammed.labadi@gmail.com>
2023-05-21 13:32:31 +0300

    used divs instead of input

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-const input = document.querySelector("input");
-const addBtn = document.getElementById("addition");
-const clearBtn = document.getElementById("clear");
-const equalsBtn = document.getElementById("equals");
-
-inputValues = [];
-
-addBtn.addEventListener("click", () => {
-  input.value += "+";
-  inputValues.push("+");
-});
-
-equalsBtn.addEventListener("click", () => {
-  for (i in inputValues) {
-  }
-});
-
-clearBtn.addEventListener("click", () => {
-  input.value = "";
-});
+const currOperation = document.querySelector(".current-operations");
+const plusBtn = document.getElementById("addition");
 
 function addNumsToInput(num) {
-  input.value += num;
-  inputValues.push(num);
+  currOperation.innerText += num;
 }
