5ad55b60cd67d1ba15a8d8f7fd7fa78b750d3bd8
Ali_Labadi <ali.mohammed.labadi@gmail.com>
2023-05-21 14:40:25 +0300

    made plus operand functional

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const currOperation = document.querySelector(".current-operations");
+const prevOperation = document.querySelector(".prev-operations");
 const plusBtn = document.getElementById("addition");
+const equalsBtn = document.getElementById("equals");
+const clearBtn = document.getElementById("clear");
 
 function addNumsToInput(num) {
   currOperation.innerText += num;
 }
+
+plusBtn.addEventListener("click", () => {
+  leftSideValue = currOperation.innerText;
+  currOperation.innerText += "+";
+  prevOperation.innerText = currOperation.innerText;
+  currOperation.innerText = "";
+});
+
+equalsBtn.addEventListener("click", () => {
+  rightSideValue = currOperation.innerText;
+  currOperation.innerText = "";
+  result = parseInt(leftSideValue) + parseInt(rightSideValue);
+  prevOperation.innerText += rightSideValue;
+  currOperation.innerText = result;
+});
+
+clearBtn.addEventListener("click", () => {
+  currOperation.innerText = "";
+  prevOperation.innerText = "";
+});
